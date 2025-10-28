b871cfbca44ae7795211860edba2c7fece86bd89
kokoyasa <krms0727@gmail.com>
2025-10-28 19:13:35 +0900

    Update StaticLib.c

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -19,7 +19,6 @@ int sum(int num, int* array)
 }
 
 // num個の整数の平均を得る
-int average(int num, int* array)
-{
-	return 0x7fffffff;// ■未実装
+int average(int num, int* array){
+	return sum(num, array) /num;
 }
